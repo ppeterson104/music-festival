130a7b79cfe1c7fda0153f6f1ab9685874ea6d9c
Pete Peterson <pete.peterson@coxautoinc.com>
2023-08-21 09:21:29 -0400

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: ["./src/**/*.{html,js}"],
-  theme: {},
-  plugins: [],
+  theme: {
+    extend: {
+      keyframes: {
+        wavey: {
+          "0%, 100%": {
+            transform: "scaleY(0.5)"
+          },
+          "50%": {
+            transform: "scaleY(1.5)"
+          }
+        }
+      },
+      animation: {
+        wavey: "wavey 1s linear infinite"
+      }
+    }
+  },
+  plugins: [require("./plugins/openVariant"), require("./plugins/animationDelay"), require("./plugins/tableCaption"), require("./plugins/buttonPlugin")],
 };
